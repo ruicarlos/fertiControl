389cb6445448ca745577f6a419b6442d6b4db7a9
RuiCarlos <ruicarlos3g.rc@gmail.com>
2025-07-03 08:27:53 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+# Carrega o modelo treinado
+modelo = joblib.load("modelo_dosagem.pkl")
+
+# Define o app
+app = FastAPI(title="API de Dosagem de Fertilizante")
+
+# Define os dados esperados
+class FertilizanteInput(BaseModel):
+    N: int
+    P: int
+    K: int
+    volume: float
+
+# Rota principal de predição
+@app.post("/prever_dosagem")
+def prever_dosagem(dados: FertilizanteInput):
+    entrada = np.array([[dados.N, dados.P, dados.K, dados.volume]])
+    pred = modelo.predict(entrada)[0]
+    return {
+        "amonia (g)": round(pred[0], 2),
+        "fosfato (g)": round(pred[1], 2),
+        "potassio (g)": round(pred[2], 2)
+    }
